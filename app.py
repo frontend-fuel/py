c507e1dadb44f9d4ee1face7679230babfdc307a
frontend-fuel <neverfail2026@gmail.com>
2025-03-28 22:37:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import threading
 import pandas as pd
 import numpy as np
-from flask import Flask, request, render_template, jsonify
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
